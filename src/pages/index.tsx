3941935fd763d935ca4d8e10cd73b8df1c07941f
Glenn Francis <xglennfrancisx@gmail.com>
2023-04-15 09:33:47 +1000

    feat: with posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { api } from "@/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser();
-  console.log(user);
+
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
+
   return (
     <>
       <Head>
@@ -16,6 +20,14 @@ const Home: NextPage = () => {
       <main>
         {!user.isSignedIn && <SignInButton />}
         {user.isSignedIn && <SignOutButton />}
+
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>
+              <h1>{post.content}</h1>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
